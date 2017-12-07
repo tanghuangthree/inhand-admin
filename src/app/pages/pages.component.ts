14e8bb1f16e76a7ebe14983dbd1fc226377dd0e4
yuhuangdada <930303github>
2017-12-07 18:12:52 +0800

    Function Or Bug: update pages and theme Symptom: N/A BugID:  N/A Root Cause: N/A Solution: N/A Note:  N/A

---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pages',
+  template: ``,
+  styles: []
+})
+export class PagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
